1bf7cd4cea0c05d90cc6857f07d5bf0252839cc1
Jefferey Neuffer <jeffereyneuffer@gmail.com>
2024-05-19 18:34:44 +1000

    add dart file headers

--- lib/mtgjson/dataModel/searchable_card.dart
+++ lib/mtgjson/dataModel/searchable_card.dart
@@ -1,3 +1,23 @@
+/*
+ * Magic Deck Manager
+ * searchable_card.dart
+ * 
+ * Copyright (C) 2023 - 2024 Jefferey Neuffer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'package:json_annotation/json_annotation.dart';
 import 'package:magic_deck_manager/mtgjson/dataModel/leadership_skills.dart';
 import 'package:string_normalizer/string_normalizer.dart';
--- lib/ui/deck/deck_page.dart
+++ lib/ui/deck/deck_page.dart
@@ -1,6 +1,25 @@
+/*
+ * Magic Deck Manager
+ * deck_page.dart
+ * 
+ * Copyright (C) 2023 - 2024 Jefferey Neuffer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'dart:math';
 import 'package:collection/collection.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_enhanced_flexible_space_bar/flutter_enhanced_flexible_space_bar.dart';
 import 'package:gap/gap.dart';
